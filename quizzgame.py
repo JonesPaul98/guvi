d29152869468d20a7aa91699e82ab8575a068ed8
JonesPaul98 <59199480+JonesPaul98@users.noreply.github.com>
2020-01-22 22:14:50 +0530

    Create quizzgame.py

---
+++ quizzgame.py
@@ -0,0 +1,25 @@
+print("level 1")
+print("when did we get freedom?")
+option=(input("1.1947 2.1950 3.1944 4.1951\n"))
+if(option!='1'):
+  print("the answer is wrong")
+else:
+  print("the answer is correct")
+print("what is color of eye?")
+option=(input("1.blue 2.green 3.yellow 4.black\n"))
+if option!=4:
+  print("answer is wrong")
+else:
+  print("correct answer")
+q=input("do you want to quit?,press q..\t")
+if q=='q':
+  exit
+else:
+  print("you have entered level 2")
+  print("when did india win WC?")
+  option=(input("1.2011 2.2010 3.2000 4.2005\n"))
+  if option!=1:
+    print("wrong answer")
+  else:
+    print("you are correct")
+  print("You have sucessfully completed the quizz!!")
