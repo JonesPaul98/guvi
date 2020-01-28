4c5a1a59d05b0b594bff5ec39f33f59cd186c85a
JonesPaul98 <59199480+JonesPaul98@users.noreply.github.com>
2020-01-28 20:56:36 +0530

    Create looping.py

---
+++ looping.py
@@ -0,0 +1,10 @@
+#Perimeter of rectangle-While loop
+i=1
+n=int(input("enter the range:"))
+while i<=n:
+  l=int(input("enter the length:"))
+  b=int(input("enter the breadth:"))
+  perimeter=2*(l+b)
+  i=i+1
+  print("Perimeter of rectangle:",perimeter)
+  
