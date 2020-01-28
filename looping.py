1de9551cdae8451e93775e7ee876f35a1d593e64
JonesPaul98 <59199480+JonesPaul98@users.noreply.github.com>
2020-01-28 21:05:48 +0530

    Update looping.py

--- looping.py
+++ looping.py
@@ -17,3 +17,16 @@ while i<=n:
   area=(l*b)
   i=i+1
   print("area of rectangle:",area)
+
+  # circle operation-While loop
+  i=1
+n=int(input("enter the range:"))
+while i<=n:
+  r=int(input("enter the radius:"))
+  circumference=2*3.14*r
+  area=3.14*r*r
+  diameter=2*r
+  i=i+1
+  print("area of circle:",area)
+  print("area of circle:",circumference)
+  print("area of circle:",diameter)
